c7926891e64e676e081b0206b9f76117a6701995
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-06-21 20:15:24 +0300

    all tests passing

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []  # Initialize knowledge as an empty list
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+
